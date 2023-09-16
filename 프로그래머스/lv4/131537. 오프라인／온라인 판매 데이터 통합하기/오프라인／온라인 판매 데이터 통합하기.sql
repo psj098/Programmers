602939bd322746b304314b05f1bb150023e7c115
Sung Joon Park <park_sungjoon@hotmail.com>
2023-09-16 23:13:12 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,27 +1,12 @@
 -- 코드를 입력하세요
 
-
-# SELECT DATE_FORMAT(onsale.sales_date, '%Y-%m-%d'), onsale.product_id, user_id, onsale.sales_amount
-# FROM online_sale AS onsale
-# WHERE DATE_FORMAT(sales_date, '%Y-%m') ='2022-03'
-
-# UNION ALL
-
-# SELECT DATE_FORMAT(offsale.sales_date, '%Y-%m-%d'), offsale.product_id, NULL AS user_id, offsale.sales_amount
-# FROM offline_sale AS offsale
-# WHERE DATE_FORMAT(sales_date, '%Y-%m') = '2022-03'
-
-# ORDER BY 1, 2, 3
-
-
-
-SELECT DATE_FORMAT(sales_date, '%Y-%m-%d') AS SALES_DATE, product_id, user_id, sales_amount
+SELECT DATE_FORMAT(sales_date, '%Y-%m-%d') AS SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
 FROM online_sale
 WHERE sales_date BETWEEN '2022-03-01' AND '2022-03-31' 
 
 UNION ALL 
 
-SELECT DATE_FORMAT(sales_date, '%Y-%m-%d') AS SALES_DATE, product_id, NULL AS user_id, sales_amount 
+SELECT DATE_FORMAT(sales_date, '%Y-%m-%d') AS SALES_DATE, PRODUCT_ID, NULL AS USER_ID, SALES_AMOUNT
 FROM offline_sale 
 WHERE sales_date BETWEEN '2022-03-01' AND '2022-03-31'
 
