f92eac707331b7caeecc76935e152e14c8e0fd39
Sung Joon Park <park_sungjoon@hotmail.com>
2023-09-27 11:28:10 +0900

    [unrated] Title: 문자열 섞기, Time: 0.01 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.py
@@ -1,8 +1,11 @@
-def solution(str1, str2):
-    answer = "" 
+def solution(str1, str2): 
+    if len(str1) != len(str2): 
+        raise ValueError("Both strings should have the same length")
+        
+    answer = [] 
     
     for i in range(len(str1)): 
-        answer += str1[i] 
-        answer += str2[i] 
-        
-    return answer
+        answer.append(str1[i]) 
+        answer.append(str2[i]) 
+    
+    return ''.join(answer) 
