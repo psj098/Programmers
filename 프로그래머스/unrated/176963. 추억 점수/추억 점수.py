9a0ee6aae6e99e2023e3d88e32509b38fa7b575e
Sung Joon Park <park_sungjoon@hotmail.com>
2023-09-28 16:17:00 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -1,12 +1,5 @@
 def solution(name, yearning, photo):
     score_dict = dict(zip(name, yearning)) 
-    
-    result = [] 
-    for name_list in photo: 
-        sum = 0 
-        for person in name_list: 
-            if person in score_dict: 
-                sum += score_dict[person] 
-        result.append(sum) 
-    
-    return result
+    result = [sum(score_dict[person] for person in name_list if person in score_dict) for name_list in photo]
+
+    return result 
