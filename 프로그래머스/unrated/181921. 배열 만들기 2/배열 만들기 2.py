eb04197d60e66e2deeebe7ac471eafcbbaee65ef
Sung Joon Park <park_sungjoon@hotmail.com>
2023-10-24 14:09:13 +0900

    [level unrated] Title: 배열 만들기 2, Time: 19.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -2,10 +2,10 @@ def solution(l, r):
     result = []
     
     for num in range(l, r+1):
-        if set(str(num)) <= {"0", "5"}:  # "0"과 "5"로만 이루어진 경우
+        if set(str(num)) <= {"0", "5"}:  
             result.append(num)
     
-    if not result:  # 만약 결과가 없다면
+    if not result:  
         return [-1]
     
     return result
