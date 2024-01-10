78dff0ec8c78580ffad31daa5d7ea6ed51cb10a4
Sung Joon Park <park_sungjoon@hotmail.com>
2024-01-10 23:10:20 +0900

    [level unrated] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,17 @@
+def solution(num_list):
+    total_operations = 0
+
+    for num in num_list:
+        operations = 0
+
+        while num != 1:
+            if num % 2 == 0:
+                num /= 2
+            else:
+                num -= 1
+                num /= 2
+            operations += 1
+
+        total_operations += operations
+
+    return total_operations
